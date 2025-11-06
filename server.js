8d7ea3b6971fb0f552aa02ef45308aacf4053b8b
Faiq Rafii <rafiiwahyudi25@gmail.com>
2025-11-06 14:01:13 +0700

    restart

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Connected To API With MongoDB");
+});
+
 const loginRouter = require("./routes/login");
 const registerRouter = require("./routes/register");
 const halaqohRouter = require("./routes/halaqoh");
@@ -13,38 +17,21 @@ const presensiRouter = require("./routes/presensi");
 const santriRouter = require("./routes/santri");
 const setoranRouter = require("./routes/setoran");
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000,
-  bufferCommands: false,
-};
-
-async function startServer() {
-  try {
-    await mongoose.connect(process.env.MONGO_URL, options);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(4000);
     console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-    // Register routers only after successful connection
-    app.use("/login", loginRouter);
-    app.use("/register", registerRouter);
-    app.use("/halaqoh", halaqohRouter);
-    app.use("/presensi", presensiRouter);
-    app.use("/santri", santriRouter);
-    app.use("/setoran", setoranRouter);
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+app.use("/halaqoh", halaqohRouter);
+app.use("/presensi", presensiRouter);
+app.use("/santri", santriRouter);
+app.use("/setoran", setoranRouter);
 
-    app.listen(4000, () => {
-      console.log("Server is running on port 4000");
-    });
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
-
-startServer();
-
-app.get("/", (req, res) => {
-  res.send("Server is running with MongoDB");
-});
-
-module.exports = app;
+module.exports=app
