f3d2666d58cd54d3a5b13c25da445c3965d96b3f
Faiq Rafii <rafiiwahyudi25@gmail.com>
2025-11-06 13:06:28 +0700

    fix server

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Connected To API");
+  res.send("Connected To API With MongoDB");
 });
 
 const loginRouter = require("./routes/login");
-const registerRouter=require("./routes/register")
-const halaqohRouter=require("./routes/halaqoh")
-const presensiRouter=require("./routes/presensi")
-const santriRouter=require("./routes/santri")
-const setoranRouter=require('./routes/setoran')
+const registerRouter = require("./routes/register");
+const halaqohRouter = require("./routes/halaqoh");
+const presensiRouter = require("./routes/presensi");
+const santriRouter = require("./routes/santri");
+const setoranRouter = require("./routes/setoran");
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-  app.listen(4000);
-  console.log("Connected to MongoDB");
-}).catch((error)=>{
-  console.log(error);
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(4000);
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use("/login", loginRouter);
-app.use("/register",registerRouter)
-app.use("/halaqoh",halaqohRouter)
-app.use("/presensi",presensiRouter)
-app.use("/santri",santriRouter)
-app.use('/setoran',setoranRouter)
+app.use("/register", registerRouter);
+app.use("/halaqoh", halaqohRouter);
+app.use("/presensi", presensiRouter);
+app.use("/santri", santriRouter);
+app.use("/setoran", setoranRouter);
+
+module.exports.handler=async (event,context)=>{
+  return app(event,contex)
+}
